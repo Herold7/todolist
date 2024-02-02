b5982ee8e23db1276b9612ef0a245802704c625b
Yankhoba <bagayoko.yan@gmail.com>
2024-02-02 12:22:53 +0100

    🔥 Gestion de la redirection

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,24 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_page', methods:['GET'])]
-    public function index(AuthorizationCheckerInterface $authorizationChecker ): Response
+    public function index(): Response
     {
-     // Vérifier si l'utilisateur est connecté
-     if (!$authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
-        // Utilisateur non connecté, rediriger vers la page d'inscription
-        return $this->redirectToRoute('app_login');
-    }
-
-    // Utilisateur connecté, rediriger vers le tableau de bord
-    return $this->redirectToRoute('app_dashboard');
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
     }
 
     #[Route('/home', name: 'app_home', methods:['GET'])]
